4325c7ae0802bafa16d241ea11b35f4ba793d9da
darya.zahiri@gmail.com <darya.zahiri@gmail.com>
2025-12-25 17:57:41 +0330

    simple client completed

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,4 +1,24 @@
 package client;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class Client {
+    public static void main(String[] args) throws Exception {
+        String serverIP = "127.0.0.1";
+        int serverPort = 12345;
+        DatagramSocket socket = new DatagramSocket();
+        String message = "Hello,I am Server";
+        byte[] data = message.getBytes();
+        DatagramPacket packet = new DatagramPacket(data, data.length, InetAddress.getByName(serverIP), serverPort);
+        socket.send(packet);
+
+        byte[] buffer = new byte[1024];
+        DatagramPacket responsePacket = new DatagramPacket(buffer, buffer.length);
+        socket.receive(responsePacket);
+        String response = new String(responsePacket.getData(), 0, responsePacket.getLength());
+        System.out.println("Received from server: " + response);
+        socket.close();
+    }
 }
